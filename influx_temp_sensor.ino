a687e9c461ceaad8e8133c30df89967da330900f
Erik Brewster <erik@erikbrewster.com>
2022-02-01 17:35:49 -0800

    added watchdog, reviewed stability issues

--- influx_temp_sensor.ino
+++ influx_temp_sensor.ino
@@ -2,18 +2,12 @@
 #include <EEPROM.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <esp_task_wdt.h>
+#include <secrets.h>
 
-#define INFLUXDB_DB_NAME "xxxx"
-#define INFLUXDB_URL "http://192.168.xxx.xxx:8086/write?db=environment"
-#define INFLUXDB_USER "xxxx"
-#define INFLUXDB_PASSWORD "xxxx"
+#define WDT_TIMEOUT 100
 
 DHTesp dht;
-
-const char* ssid = "xxxx"; // wifi ssid
-const char* password = "xxxx"; // wifi password
-const char* hostname = "xxxx"; // hostname of device
-const char* location = "xxxx"; // used to prefix the data
 
 void tempTask(void *pvParameters);
 bool getTemperature();
@@ -39,9 +33,11 @@ String readString(char add)
   while(k != '\0' && len<500)   //Read until null character
   {    
     k=EEPROM.read(add+len);
+    Serial.println(k);
     data[len]=k;
     len++;
   }
+  Serial.println("null to finish");
   data[len]='\0';
   return String(data);
 }
@@ -92,8 +88,8 @@ void scanNetworks() {
 
 void connectToNetwork() {
   while (true){
-    WiFi.setHostname(hostname);
-    WiFi.begin(ssid, password);
+    WiFi.setHostname(HOSTNAME);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   
     for(int i = 0; i < 10; i++){
       if (WiFi.status() != WL_CONNECTED) {
@@ -127,51 +123,76 @@ bool initTemp() {
 }
 
 void sendDataToInflux(char* tag, float value) {
+  int streamReturn;
+  
   Serial.println("4: " + String(tag));
   Serial.println("5: " + String(value));
   if(WiFi.status() != WL_CONNECTED) {
     connectToNetwork();
   }
+  Serial.println("verified connected to wifi");
   String debugString = "";
   debugString += String(tag);
   debugString += String(value);
   HTTPClient http;
   String serverPath = INFLUXDB_URL;
   debugString += ("start send: " + String(INFLUXDB_URL));
+  Serial.println("6: " + debugString);
   http.begin(serverPath);
   debugString += ("begin done: " + String(serverPath));
+  Serial.println("7: " + debugString);
   http.addHeader("Content-Type", "text/plain");
   debugString += ("header added");
+  Serial.println("8: " + debugString);
   http.setAuthorization(INFLUXDB_USER, INFLUXDB_PASSWORD);
   debugString += ("auth set: " + String(INFLUXDB_USER) + ", " + String(INFLUXDB_PASSWORD));
   
+  Serial.println("9: " + debugString);
   String influxData = "";
   influxData += "hrv " + String(tag) + "=" + String(value);
 
   debugString += ("data: " + String(influxData));
+  
+  Serial.println("10: " + debugString);
 
   // The first attempt to send returns -1
   int httpCode = -1;
   while(httpCode == -1){
     debugString += ("influx send loop: " + influxData);
     httpCode = http.POST(influxData);
+    debugString += String(httpCode);
+    Serial.println("11: " + debugString);
     debugString += ("post done");
-    http.writeToStream(&Serial);
+    Serial.println("11.1: " + debugString);
+    //streamReturn = http.writeToStream(&Serial);
+    //debugString += String(streamReturn);
+    Serial.println("12: " + debugString);
+    /*
+    
     debugString += ("httpCode: " + String(httpCode));
     String response = http.getString();
     debugString += ("response: " + response);
+    
+    Serial.println("3: " + debugString);
 
     if (httpCode != 204) {
       Serial.println(debugString);
     }
+    else {
+      Serial.println("204");
+    }
+    */
   }
   http.end();
+  
+  Serial.println("14: " + debugString);
 }
 
 void setup()
 {
   EEPROM.begin(512);
   Serial.begin(115200);
+  delay(2000);
   Serial.println("");
   Serial.println("");
   Serial.println("");
@@ -181,21 +202,30 @@ void setup()
   Serial.println("");
   Serial.println("");
   Serial.println("--------- Starting ---------");
-  deviceName = readString(0);
+  //deviceName = readString(0);
+  deviceName = "ngarage";
   Serial.println("deviceName: " + String(deviceName));
   initTemp();
 
+  Serial.println("Scanning networks");
   scanNetworks();
+  Serial.println("First connect to network");
   connectToNetwork();
 
+  Serial.println("MAC Address");
   Serial.println(WiFi.macAddress());
+  Serial.println("Local IP");
   Serial.println(WiFi.localIP());
 
-//  WiFi.disconnect(true);
-//  Serial.println("Disconnecting: " + String(WiFi.localIP()));
+  //  WiFi.disconnect(true);
+  //  Serial.println("Disconnecting: " + String(WiFi.localIP()));
 
   // Wait 2 seconds to let system settle down
   delay(2000);
+
+  Serial.println("Staring watchdog");
+  esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL); //add current thread to WDT watch
 }
 
 void loop() {
@@ -208,6 +238,8 @@ void loop() {
 
   sendDataToInflux("ngarage_temp", temp);
   sendDataToInflux("ngarage_humidity", newValues.humidity);
+  Serial.println("Resetting WDT");
+  esp_task_wdt_reset();
   int sleepValue = 60000;
   
   Serial.println("waiting " + String(sleepValue));
